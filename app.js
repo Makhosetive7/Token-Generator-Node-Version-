4df871e4560d9bb99f3f22b0a267142088a85d5a
makhosetive7 <sibandamakhosetive7@gmail.com>
2025-07-01 04:07:25 +0200

    finished project setup

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./config/database.js";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+connectDB().then(() => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () =>
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+  );
+})
+
+export default app
+
